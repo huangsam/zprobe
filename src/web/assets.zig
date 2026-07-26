e708ab51f02ff68bb559eac9b17a7909faa24b6d
Samuel Huang <samhuang91@gmail.com>
2026-07-26 08:22:20 -0700

    Remove stray line in asset pipeline

--- src/web/assets.zig
+++ src/web/assets.zig
@@ -31,5 +31,4 @@ const js_notes = @embedFile("js/notes.js");
 const js_drawer = @embedFile("js/drawer.js");
 const js_insights = @embedFile("js/insights.js");
 const js_main = @embedFile("js/main.js");
-
 pub const app_js = js_state ++ js_utils ++ js_filters ++ js_api ++ js_table ++ js_grid ++ js_notes ++ js_drawer ++ js_insights ++ js_main;
